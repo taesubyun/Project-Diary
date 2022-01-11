05ff42cde9fe029f5f2089a4f1b470aadc5a4c54
Taesub Yun <taesubyun@icloud.com>
2022-01-11 18:26:08 +0900

    feat : Home 컴포넌트 DiaryItem과 CSS 마무리

--- diary/src/components/DiaryItem.js
+++ diary/src/components/DiaryItem.js
@@ -1,9 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import Button from './../components/Button';
 const DiaryItem = ({ id, emotion, content, date }) => {
-    // const env = process.env;
-    // env.PUBLIC_URL = env.PUBLIC_ || '';
+    const strDate = new Date(parseInt(date)).toLocaleDateString();
+    const navigate = useNavigate();
+    const goDetail = () => {
+        navigate(`/diary/${id}`);
+    };
+    const goEdit = () => {
+        navigate(`/edit/${id}`);
+    };
+
     return (
         <div className='DiaryItem'>
             <div
+                onClick={goDetail}
                 className={[
                     'emotion_img_wrapper',
                     ` emotion_img_wrapper_${emotion}`,
@@ -14,11 +24,15 @@ const DiaryItem = ({ id, emotion, content, date }) => {
                         process.env.PUBLIC_URL + `assets/emotion${emotion}.png`
                     }
                 ></img>
-                <div className='info_wrapper'>
-                    <div className='diary_date'></div>
-                    <div className='diary_content_preview'></div>
+            </div>
+            <div onClick={goDetail} className='info_wrapper'>
+                <div className='diary_date'>{strDate}</div>
+                <div className='diary_content_preview'>
+                    {content.slice(0.25)}
                 </div>
-                <div></div>
+            </div>
+            <div className='btn_wrapper'>
+                <Button onClick={goEdit} text={'수정하기'} />
             </div>
         </div>
     );
